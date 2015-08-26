0abadeaebc2d940d20dc89cac312cc3994362ffb
Tyler Guillen <tyguillen@gmail.com>
2015-08-25 20:52:13 -0700

    complete chapter 4 of 'beginning ruby'

---
+++ analyzer.rb
@@ -0,0 +1,36 @@
+stopwords = %w{the a by on for of are with just but and to the my I has some in}
+lines = File.readlines(ARGV[0])
+line_count = lines.size
+text = lines.join
+
+total_characters = text.length
+total_characters_nospaces = text.gsub(/\s+/, '').length
+
+words = text.split
+paragraphs = text.split(/\n\n/)
+sentences = text.split(/\.|\?|!/)
+
+word_count = words.length
+paragraph_count = paragraphs.length
+sentence_count = sentences.length
+
+keywords = words.select { |word| !stopwords.include? word }
+keyword_ratio = ((keywords.length.to_f / words.length.to_f) * 100).to_i
+
+sentences = text.gsub(/\s+/, ' ').strip.split(/\.|\?|!/)
+sentences_sorted = sentences.sort_by { |sentence| sentence.length }
+one_third = sentences_sorted.length / 3
+ideal_sentences = sentences_sorted.slice(one_third, one_third + 1)
+ideal_sentences = ideal_sentences.select { |sentence| sentence =~ /is|are/ }
+
+puts "#{line_count} lines"
+puts "#{total_characters} characters"
+puts "#{total_characters_nospaces} characters excluding spaces"
+puts "#{word_count} words"
+puts "#{paragraph_count} paragraphs"
+puts "#{sentence_count} sentences"
+puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
+puts "#{word_count / sentence_count} words per sentence (average)"
+puts "Percent keywords: #{keyword_ratio}%"
+puts "\nSummary:\n\n" + ideal_sentences.join(". ")
+puts "-- End of analysis"
